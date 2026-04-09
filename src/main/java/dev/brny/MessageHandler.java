793f0577570fed13aa2a10aa3230d56358c15bcc
angrypig555 <brny@brny.dev>
2026-04-09 11:41:20 +0100

    add encryption into messageHandler

--- src/main/java/dev/brny/MessageHandler.java
+++ src/main/java/dev/brny/MessageHandler.java
@@ -1,9 +1,23 @@
 package dev.brny;
 
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Base64;
 
+import com.google.crypto.tink.*;
+import com.google.crypto.tink.config.TinkConfig;
+import com.google.crypto.tink.aead.AeadConfig;
+import com.google.crypto.tink.hybrid.HybridDecryptFactory;
+import com.google.crypto.tink.hybrid.HybridEncryptFactory;
+import com.google.crypto.tink.hybrid.HybridKeyTemplates;
+
 public class MessageHandler {
+    private KeysetHandle privateKeysetHandle;
+    private KeysetHandle publicKeysetHandle;
+    private ArrayList<KeysetHandle> peer_keys = new ArrayList<>();
+    private KeysetHandle curr_pubkey;
+
     public String cleanup_msg(String message_raw) {
         String message = decode(message_raw);
         int headerLen = Protocol.header.length();
@@ -28,5 +42,27 @@ public class MessageHandler {
         String message =  message_raw.substring(headerLen, message_raw.length() - headerLen);
         byte[] decoded = Base64.getDecoder().decode(message);
         return new String(decoded);
+    }
+    public void crypt_init() throws GeneralSecurityException {
+        System.out.println("[OK] Generating encryption keypair");
+        TinkConfig.register();
+        privateKeysetHandle = KeysetHandle.generateNew(HybridKeyTemplates.ECIES_P256_HKDF_HMAC_SHA256_AES128_CTR_HMAC_SHA256);
+        publicKeysetHandle = privateKeysetHandle.getPublicKeysetHandle();
+    }
+    public byte[] encrypt(String message) throws GeneralSecurityException {
+        try {
+            HybridEncrypt hybridEncrypt = HybridEncryptFactory.getPrimitive(curr_pubkey);
+            return hybridEncrypt.encrypt(message.getBytes(), Protocol.header.getBytes());
+        } catch (GeneralSecurityException e) {
+            throw e;
+        }
+    }
+    public String decrypt(byte[] message) throws GeneralSecurityException {
+        try {
+            HybridDecrypt hybridDecrypt = HybridDecryptFactory.getPrimitive(privateKeysetHandle);
+            return new String(hybridDecrypt.decrypt(message, Protocol.header.getBytes()));
+        } catch (GeneralSecurityException e) {
+            throw e;
+        }
     }
 }
